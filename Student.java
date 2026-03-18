f5c7502f1cc695141ed116164bedb7d4575ebc36
root <root@ip-172-31-31-253.ec2.internal>
2026-03-18 14:20:13 +0000

    lines-removed

--- Student.java
+++ Student.java
@@ -7,15 +7,5 @@ public class Student {
         System.out.println("My Name is: " + sname);
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
-        System.out.println("=======================");
-        System.out.println("JENKINS-POLL SCM CONCEPT");
-        System.out.println("JENKINS-POLL SCM CONCEPT"); 
-        System.out.println("JENKINS-POLL SCM CONCEPT");
-        System.out.println("=======================");
-        System.out.println("=======================");
-        System.out.println("=======================");
-        System.out.println("=======================");
     }
 }
-       
-       
